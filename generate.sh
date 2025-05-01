7bebe773183d9f83278aff7e45dda66b088ff6aa
Ty Book <ty@neynar.com>
2025-05-01 19:12:14 -0400

    Remove all npm stuff and make build/test scripted

--- generate.sh
+++ generate.sh
@@ -1,9 +1,23 @@
-#!/bin/bash -eu -o pipefail
-
+#!/bin/bash
+set -eux -o pipefail
 export GO_POST_PROCESS_FILE='gofmt -w'
 
 rm -rf ./api
-npx @openapitools/openapi-generator-cli generate --enable-post-process-file --git-user-id neynarxyz --git-repo-id go-sdk -i ./oas/src/v2/spec.yaml -g go -o ./api --config ./openapi-generator-config.api.json --openapi-normalizer 'SIMPLIFY_ONEOF_ANYOF=false'
+npx --yes @openapitools/openapi-generator-cli generate --enable-post-process-file --git-user-id neynarxyz --git-repo-id go-sdk -i ./oas/src/v2/spec.yaml -g go -o ./api --config ./openapi-generator-config.api.json --openapi-normalizer 'SIMPLIFY_ONEOF_ANYOF=false'
+(
+    cd ./api
+    go get github.com/stretchr/testify/assert
+    go get golang.org/x/net/context
+    go build
+    go test ./...
+)
 
 rm -rf ./hub-api
-npx @openapitools/openapi-generator-cli generate --enable-post-process-file --git-user-id neynarxyz --git-repo-id go-sdk -i ./oas/src/hub-rest-api/spec.yaml -g go -o ./hub-api --config ./openapi-generator-config.hub-api.json
+npx --yes @openapitools/openapi-generator-cli generate --enable-post-process-file --git-user-id neynarxyz --git-repo-id go-sdk -i ./oas/src/hub-rest-api/spec.yaml -g go -o ./hub-api --config ./openapi-generator-config.hub-api.json
+(
+    cd ./hub-api
+    go get github.com/stretchr/testify/assert
+    go get golang.org/x/net/context
+    go build
+    go test ./...
+)
